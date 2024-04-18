ae5356832de020b2826d24819b63320da368b1fd
yordanpetkov1990 <79831742+yordanpetkov1990@users.noreply.github.com>
2024-04-18 17:11:48 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server(1234);
         server.run();
-//        int[] arr = {1,2,3,4,5,6};
-//        System.out.println(binary_search(arr, 0, arr.length - 1, 7));
-//
-//
-   }
-    public static int binary_search(int[] arr,int lf,int rt,int x){
-        if(lf>rt){
-            return 0;
-        }
-        int m = (lf+rt) /2;
-        if(arr[m] == x){
-            return m;
-        }
-        if(lf==rt){
-            return 0;
-        }
-        if(arr[m] > x){
-            return binary_search(arr,lf,m-1,x);
-        }
-        return binary_search(arr,m+1,rt,x);
     }
-    }
+}
